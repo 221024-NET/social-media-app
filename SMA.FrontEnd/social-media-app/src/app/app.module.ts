017910a51f72cb8c079fb72b519d4e53bb5b6167
Katherine <73628642+KatherineAWard@users.noreply.github.com>
2022-12-07 14:18:41 -0800

    Add Register, Feed, and Profile pages. Link Register and Login

--- SMA.FrontEnd/social-media-app/src/app/app.module.ts
+++ SMA.FrontEnd/social-media-app/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { FeedPageComponent } from './components/feed-page/feed-page.component';
+import { ProfilePageComponent } from './components/profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    RegisterPageComponent,
+    FeedPageComponent,
+    ProfilePageComponent
   ],
   imports: [
     BrowserModule,
