f891e22dbc64a2542b38d40469beb5038419dc29
Calvin Tan <calvintan607@gmail.com>
2022-12-14 10:56:08 -0800

    basic profile added

--- SMA.FrontEnd/social-media-app/src/app/app.module.ts
+++ SMA.FrontEnd/social-media-app/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { LoginPageComponent } from './components/login-page/login-page.component
 import { RegisterPageComponent } from './components/register-page/register-page.component';
 import { FeedPageComponent } from './components/feed-page/feed-page.component';
 import { ProfilePageComponent } from './components/profile-page/profile-page.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -20,6 +20,8 @@ import { ProfilePageComponent } from './components/profile-page/profile-page.com
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- SMA.FrontEnd/social-media-app/src/app/components/profile-page/profile-page.component.ts
+++ SMA.FrontEnd/social-media-app/src/app/components/profile-page/profile-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../classes/user';
+import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.css']
 })
-export class ProfilePageComponent {
+export class ProfilePageComponent implements OnInit{
+  profileForm: any;
+  editingMode: boolean = false;
+  user: User = new User(1, "CalvinUser", "password", "FirstName", "LastName", 123);
 
+  ngOnInit(): void {
+    
+  }
+
+  toggleEditingMode() {
+    this.editingMode = true;
+  }
+
+  submitChanges(data: any) {
+    console.log("Changes submitted");
+    this.user.first_name = data.firstName;
+    this.user.last_name = data.lastName;
+    this.user.phone_number = data.phoneNumber
+    console.warn(data);
+    this.editingMode = false;
+  }
 }
