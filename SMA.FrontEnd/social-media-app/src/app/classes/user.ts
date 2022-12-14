e99b1e78ec57f0cec8fb1716beadc5369fe5c4bb
Katherine <73628642+KatherineAWard@users.noreply.github.com>
2022-12-14 14:15:45 -0800

    Add registration form validation

--- SMA.FrontEnd/social-media-app/src/app/classes/user.ts
+++ SMA.FrontEnd/social-media-app/src/app/classes/user.ts
@@ -15,10 +15,4 @@ export class User {
         this.last_name = last_name;
         this.phone_number = phone_number;
     }
-
-    constructor(user_id: number, username: string, password: string) {
-        this.user_id = user_id;
-        this.username = username;
-        this.password = password;
-    }
 }
