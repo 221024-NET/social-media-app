256241a73e81aca20ae4f35aa9afd45d24ff9118
Calvin Tan <calvintan607@gmail.com>
2022-12-15 12:29:34 -0800

    added likes model/controller, posts can now see hm liked their post

---
+++ SMA.BackEnd/SMA.BackendOps/SMA.BackendOps/Controllers/LikesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SMA.BackendOps.Models;
+
+namespace SMA.BackendOps.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LikesController : ControllerBase
+    {
+        private readonly SMADbContext _context;
+
+        public LikesController(SMADbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Likes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Like>>> GetLikes()
+        {
+            return await _context.Likes.ToListAsync();
+        }
+
+        // GET: api/Likes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Like>> GetLike(int id)
+        {
+            var like = await _context.Likes.FindAsync(id);
+
+            if (like == null)
+            {
+                return NotFound();
+            }
+
+            return like;
+        }
+
+        // PUT: api/Likes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLike(int id, Like like)
+        {
+            if (id != like.like_id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(like).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LikeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Likes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Like>> PostLike(Like like)
+        {
+            _context.Likes.Add(like);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLike", new { id = like.like_id }, like);
+        }
+
+        // DELETE: api/Likes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLike(int id)
+        {
+            var like = await _context.Likes.FindAsync(id);
+            if (like == null)
+            {
+                return NotFound();
+            }
+
+            _context.Likes.Remove(like);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LikeExists(int id)
+        {
+            return _context.Likes.Any(e => e.like_id == id);
+        }
+    }
+}
