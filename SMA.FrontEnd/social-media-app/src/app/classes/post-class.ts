d4da79aa6fa35630ca3fd5c3c0181d1858d1d6e7
SkylerOB <skyler017@revature.net>
2022-12-16 08:52:35 -0800

    post-class uses Date object for Date field

--- SMA.FrontEnd/social-media-app/src/app/classes/post-class.ts
+++ SMA.FrontEnd/social-media-app/src/app/classes/post-class.ts
@@ -1,20 +1,22 @@
 export class PostClass {
-    post_id: number = 0;
-    user_id: number = 0;
+    post_id: number;
+    user_id: number;
     //username?: string = "ABCs User";
     //user_selfie?: string = "https://www.katherineannward.com/Images/Thumbnails/25thHourTarotThumb.png";
-    content: string = "this message";
-    date: any;
+    content: string;
+    date: Date;
     //image?: string = "https://www.katherineannward.com/Images/Thumbnails/25thHourTarotThumb.png";
     //new PostClass(0,0,"name","","Oct","message","",0);
-    constructor(pid:number, uid:number, m:string, d:string, img:string) {
+    constructor(pid:number, uid:number, m:string, d:Date, img:string) {
         this.post_id = pid;
         this.user_id = uid;
         //this.username = un;
         //this.user_selfie = upic;
-        this.date = "2022-10-24";
+        this.date = d;//"2022-10-24";
         this.content = m;
         //this.image = img;
         //this.likecount = l;
     }
+
+    timeNow() { this.date = new Date(); }
 }
