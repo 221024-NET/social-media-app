d8e5010c8b32ccc6c77dfe340205d7a86b96c1bc
SkylerOB <skyler017@revature.net>
2022-12-16 09:09:16 -0800

    API WIP)

--- SMA.FrontEnd/social-media-app/src/app/components/feed-page/feed-page.component.ts
+++ SMA.FrontEnd/social-media-app/src/app/components/feed-page/feed-page.component.ts
@@ -1,22 +1,31 @@
 import { Component } from '@angular/core';
 import { PostClass } from 'src/app/classes/post-class';
 import { User } from 'src/app/classes/user';
+import { CompiledPost } from 'src/app/classes/compiled-post';
 import { DataTransferService } from 'src/app/services/data-transfer.service';
 import { PostService } from 'src/app/services/post.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-feed-page',
   templateUrl: './feed-page.component.html',
   styleUrls: ['./feed-page.component.css', '../../app.component.css']
 })
+
 export class FeedPageComponent {
-  user = new User(0, "namename", "passpass","firstfirst");
-  selected?: PostClass;
+  user = new User(1, "namename", "passpass","firstfirst","lastlast",1234567890);
+  selected?: CompiledPost;
   postset: any;
-  newpostmessage:string="";
+  newpost: any;
 
   constructor(private postal:PostService, user:DataTransferService) {
     //this.user = user.getData();
+  }
+
+  ngOnInit(): void {
+    this.newpost = new FormGroup({
+      themessage: new FormControl(""),
+    });
   }
 
   getAllPosts() {
@@ -26,10 +35,16 @@ export class FeedPageComponent {
     )
   } 
 
-  writePost(message: string) {
-    const fresh = new PostClass(0,1,"Oct","message","");
-    this.postal.makePost(fresh).subscribe(
-      (error) => {console.log(error); }
-    )
-  }
+  // writePost(message: string) {
+  //   const today = new Date();
+  //   const fresh = new PostClass(0,1,this.newpost,today,"");
+  //   this.postal.makePost(fresh).subscribe(
+  //     (response) => {console.log(response); },
+  //     (error) => {console.log(error); }
+  //   )
+  // }
+
+  // getAllFriendsPosts() {
+  //   this.postal.
+  // }
 }
