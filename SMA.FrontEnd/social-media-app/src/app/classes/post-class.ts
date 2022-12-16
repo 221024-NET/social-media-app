b2f37c476ef35ed9bea1299115b1b863cc9b4b90
SkylerOB <skyler017@revature.net>
2022-12-16 11:55:45 -0800

    removed likes count

--- SMA.FrontEnd/social-media-app/src/app/classes/post-class.ts
+++ SMA.FrontEnd/social-media-app/src/app/classes/post-class.ts
@@ -1,18 +1,14 @@
 export class PostClass {
     post_id: number;
     user_id: number;
-    //username?: string = "ABCs User";
-    //user_selfie?: string = "https://www.katherineannward.com/Images/Thumbnails/25thHourTarotThumb.png";
     content: string;
     date: Date;
     //image?: string = "https://www.katherineannward.com/Images/Thumbnails/25thHourTarotThumb.png";
-    //new PostClass(0,0,"name","","Oct","message","",0);
+
     constructor(pid:number, uid:number, m:string, d:Date, img:string) {
         this.post_id = pid;
         this.user_id = uid;
-        //this.username = un;
-        //this.user_selfie = upic;
-        this.date = d;//"2022-10-24";
+        this.date = d;
         this.content = m;
         //this.image = img;
         //this.likecount = l;
