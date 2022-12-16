de511da139e57f8d6eb0b6d1777f96ee77f95af5
SkylerOB <skyler017@revature.net>
2022-12-16 12:46:10 -0800

    reinstated image field

--- SMA.FrontEnd/social-media-app/src/app/classes/post-class.ts
+++ SMA.FrontEnd/social-media-app/src/app/classes/post-class.ts
@@ -3,14 +3,14 @@ export class PostClass {
     user_id: number;
     content: string;
     date: Date;
-    //image?: string = "https://www.katherineannward.com/Images/Thumbnails/25thHourTarotThumb.png";
+    image?: string = "https://www.katherineannward.com/Images/Thumbnails/25thHourTarotThumb.png";
 
     constructor(pid:number, uid:number, m:string, d:Date, img:string) {
         this.post_id = pid;
         this.user_id = uid;
         this.date = d;
         this.content = m;
-        //this.image = img;
+        this.image = img;
         //this.likecount = l;
     }
 
