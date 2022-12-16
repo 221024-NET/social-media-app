6542819bca1c7e0257b9c30f8a7b2d201f898440
SkylerOB <skyler017@revature.net>
2022-12-16 11:57:04 -0800

    uses dto

--- SMA.FrontEnd/social-media-app/src/app/components/PostComponents/post-summary/post-summary.component.ts
+++ SMA.FrontEnd/social-media-app/src/app/components/PostComponents/post-summary/post-summary.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CompiledPost } from 'src/app/classes/compiled-post';
 import { PostClass } from 'src/app/classes/post-class';
+import { User } from 'src/app/classes/user';
+
 
 @Component({
   selector: 'app-post-summary',
@@ -7,5 +10,10 @@ import { PostClass } from 'src/app/classes/post-class';
   styleUrls: ['./post-summary.component.css']
 })
 export class PostSummaryComponent {
-  public post: PostClass = new PostClass(0,0,"summary post",new Date(),"");
+  public _selected: CompiledPost = new CompiledPost(new User(0,"0u",""), new PostClass(0,0,"0msg",new Date(),""));
+
+  @Input()
+  public set selected(selected: CompiledPost) {
+    this._selected = selected;
+  }
 }
