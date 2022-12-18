ef52b66d75f39d7c4960384652db4d5b43d24498
Damon <damonboone99@gmail.com>
2022-12-18 14:32:41 -0800

    add validation to comments

--- SMA.FrontEnd/social-media-app/src/app/components/post-comment/post-comment.component.ts
+++ SMA.FrontEnd/social-media-app/src/app/components/post-comment/post-comment.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 import { CommentService } from 'src/app/services/comment.service';
 import { CommentClass } from '../../classes/comment-class';
 import { User } from 'src/app/classes/user';
@@ -10,7 +10,7 @@ import { DataTransferService } from 'src/app/services/data-transfer.service';
   templateUrl: './post-comment.component.html',
   styleUrls: ['./post-comment.component.css']
 })
-export class PostCommentComponent {
+export class PostCommentComponent implements OnInit {
   user = new User(0, "", "");
   
   model = new CommentClass(0, "", 0, 0);
@@ -18,24 +18,35 @@ export class PostCommentComponent {
  
   @Input() post_id: number = 0;
 
-  
+  formdata: any;
 
   constructor(private commental: CommentService, private use: DataTransferService) {
     this.user = use.findUser();
   }
 
+  ngOnInit(): void {
+    this.formdata = new FormGroup({
+      comment: new FormControl("")
+    });
+  }
+
 
   postAComment(form: NgForm) {
-    this.model.comment = form.value.comment;
-    console.log(this.model.comment);
-    this.model.post_id = this.post_id;
-    console.log(this.model.post_id);
-    this.model.user_id = this.user.user_id
-    console.log(this.model.user_id);
-    this.commental.makePost(this.model).subscribe(
-      (response) => { console.log("making a comment") },
-      (error) => { console.log(error); }
-    );
+    if (!this.formdata.valid) {
+      this.formdata.markAllAsTouched();
+    }
+    else {
+      this.model.comment = form.value.comment;
+      console.log(this.model.comment);
+      this.model.post_id = this.post_id;
+      console.log(this.model.post_id);
+      this.model.user_id = this.user.user_id
+      console.log(this.model.user_id);
+      this.commental.makePost(this.model).subscribe(
+        (response) => { console.log("making a comment") },
+        (error) => { console.log(error); }
+      );
+    }
   }
 
 
